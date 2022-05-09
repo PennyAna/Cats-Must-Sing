7ace561e6471532987b400de5e3c47afa1397560
PennyAna <lawrence.restrainedchaos@gmail.com>
2022-05-09 12:12:56 -0600

    cleared out possible bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
+import { DocumentComponent } from './document/document.component';
+import { ContactComponent } from './contact/contact.component';
+import { MessageComponent } from './message/message.component';
+import { ContactItemComponent } from './contact/contact-item/contact-item.component';
+import { DocumentDetailComponent } from './document/document-detail/document-detail.component';
+import { DocumentListComponent } from './document/document-list/document-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactComponent,
+    ContactListComponent,
+    ContactItemComponent,
+    ContactDetailComponent,
+    DocumentComponent,
+    MessageComponent,
+    DocumentDetailComponent,
+    DocumentListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
