dbedf666b06a94a9a453b3c7840f150f22fcbe1c
PennyAna <lawrence.restrainedchaos@gmail.com>
2022-05-15 15:44:46 -0600

    Works: all message-edit comp files; Failed: none, but need to complete message setup; use class videos, tutorial, et assignment details

--- src/app/message/message-edit/message-edit.component.ts
+++ src/app/message/message-edit/message-edit.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input, ViewChild, EventEmitter } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, OnInit, Input, ViewChild, EventEmitter, ElementRef, Output } from '@angular/core';
 import { Message } from '../message.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'cms-message-edit',
@@ -10,11 +10,27 @@ import { Message } from '../message.model';
 export class MessageEditComponent implements OnInit {
 
 @Input() message: Message | undefined;
+
+@ViewChild('msgText') msgText: ElementRef;
+@ViewChild('subject') subject: ElementRef;
+@Output() addMessageEvent = new EventEmitter<Message>();
+currentSender = 'Nicole Lawrence';
+currentId = '5';
 onSendMessage() {
-  
+    const subjectValue = this.subject?.nativeElement.value;
+    const msgTextValue = this.msgText?.nativeElement.value;
+
+    const message = new Message(
+      '5', 
+      subjectValue, 
+      msgTextValue, 
+      this.currentSender);
+      this.addMessageEvent.emit(message);
+      this.onClear(); 
 }
-onClear() {
-  
+onClear(): void {
+  this.subject.nativeElement.value = ' ';
+  this.msgText.nativeElement.value = ' ';
 }
   constructor() { }
 
